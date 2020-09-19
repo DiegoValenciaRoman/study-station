dd2e154ea7e610672c64ce659bd4bb63484ae9da
Manuel <m24.tapia@gmail.com>
2020-09-19 18:25:30 -0300

    Se obtuvo la información de un curso único y semestre activo.

--- backend/routes/index-routes.ts
+++ backend/routes/index-routes.ts
@@ -5,6 +5,7 @@ import * as usuariosCtrl from "../api/controllers/usuarios";
 const router = Router();
 
 router.post("/scrappInformacionUsuario", usuariosCtrl.scrappInformacionUsuario);
+router.post("/scrappCursosUsuario", usuariosCtrl.scrappCursosUsuario);
 router.post("/usuarios", usuariosCtrl.crearUsuario);
 
 export default router;
